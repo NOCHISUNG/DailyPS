774d9bb842cebd0deb8cb56fa425bd04d4a76356
1gil2 <qazxcv8640@gmail.com>
2021-08-19 11:39:14 +0900

    20210819

---
+++ Hangil/day17_8986_choi.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def f(p):
+    ret = 0
+    for i, x in enumerate(xs):
+        ret += abs(p * i - x)
+    return ret
+
+
+n = int(input())
+xs = list(map(int, input().split()))
+
+left = 0
+right = xs[n - 1]
+while right - left >= 3:
+    p1 = (left * 2 + right) // 3
+    p2 = (left + right * 2) // 3
+
+    if f(p1) < f(p2):
+        right = p2
+    else:
+        left = p1
+
+ans = sys.maxsize
+for x in range(left, right + 1):
+    ans = min(ans, f(x))
+
+print(ans)
